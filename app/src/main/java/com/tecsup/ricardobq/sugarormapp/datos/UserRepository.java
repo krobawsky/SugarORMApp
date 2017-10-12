71cc72c08a524a678864a4b3d162c29ff9774170
krobawsky <rtu2017>
2017-10-12 13:59:46 -0500

    workk

---
+++ app/src/main/java/com/tecsup/ricardobq/sugarormapp/datos/UserRepository.java
@@ -0,0 +1,37 @@
+package com.tecsup.ricardobq.sugarormapp.datos;
+
+
+import com.orm.SugarRecord;
+
+import java.util.List;
+
+public class UserRepository {
+    public static List<User> list(){
+        List<User> users = SugarRecord.listAll(User.class);
+        return users;
+    }
+
+    public static User read(Long id){
+        User user = SugarRecord.findById(User.class, id);
+        return user;
+    }
+
+    public static void create(String fullname, String email, String password){
+        User user = new User(fullname, email, password);
+        SugarRecord.save(user);
+    }
+
+    public static void update(String fullname, String email, String password, Long id){
+        User user = SugarRecord.findById(User.class, id);
+        user.setFullname(fullname);
+        user.setEmail(email);
+        user.setPassword(password);
+        SugarRecord.save(user);
+    }
+
+    public static void delete(Long id){
+        User user = SugarRecord.findById(User.class, id);
+        SugarRecord.delete(user);
+    }
+
+}
